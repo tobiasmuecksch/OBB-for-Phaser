6f4042865e9a9eea8d8cbcb4b2ffd832d3cfe042
Tobias Mücksch <tobias@muecks.ch>
2015-06-26 19:02:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# OBB-for-Phaser
+This project is about replacing Phaser.io's Arcade Physics AABB collision detection (which doesn't offer bounding box rotation) with "Oriented bounding box" collision detection or short OBB.
+
+The great benefit: OBB supports rotating bounding boxes!
+
+## Important
+This project is in a testing state. Please feel free to add pull requests and issues!
+
+## Usage
+- Download the obb.js file from the sources
+- Add it to your html right behind phaser, like this:
+```
+<script type="text/javascript" src="lib/phaser.js"></script>
+<script type="text/javascript" src="lib/obb.js"></script>
+```
+- Then in your game.js add this line in the preload function (I assume you already have started the Arcade physics)
+```
+OBB.setOBB(this.game.world);
+```
+
+
+## Bower
+As soon as this project is stable and safe to use, I will add it to bower.
+
+## License
+MIT. See the LICENSE.md file in this repository for mor information.
+
+## Use it on your own risk!
+Be aware that you use this code on your own risk. The authors of this code won't take any responsibilites for anything.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ obb.js
@@ -0,0 +1,101 @@
+var OBB = {
+
+	// Check for a collision.
+	// A and B have to be fo type Phaser.Physic.Arcade.Body 
+	collisionCheck: function  (A, B) {
+		var ARect = OBB.createRectangle(A.center, rotateVector([A.halfWidth, A.halfHeight], Phaser.Math.degToRad(A.angle)));
+		var BRect = OBB.createRectangle(B.center, rotateVector([B.halfWidth, B.halfHeight], Phaser.Math.degToRad(B.angle)));
+		
+		return OBB.isecRects(ARect, BRect);
+	},
+
+	// We assume a vector consisting of [halfWidth, halfHeight], which can be used to point from the center to the edges
+	// We need to rotate this vector in order to create the rotated rectangle
+	rotateVector: function (vector, radians) {
+		var x = vector[0] * Math.cos(radians) - vector[1] * Math.sin(radians);
+		var y = vector[0] * Math.sin(radians) + vector[1] * Math.cos(radians);
+
+		return [x, y];
+	},
+
+	// This function takes a center point (in this format [x,y]) and a rotated vector (in this format [x,y])
+	// and returns a rectangle in this format: [x1, y1, x2, y2, x3, y3, x4, y4]
+	createRectangle: function (center, vector) {
+		var x = vector[0],
+		y = vector[1],
+		
+		x1 = center[0] + x,
+		y1 = center[1] + y,
+		
+		x2 = center[0] + x,
+		y2 = center[1] - y,
+		
+		x3 = center[0] - x,
+		y3 = center[1] - y,
+		
+		x4 = center[0] - x,
+		y4 = center[1] + y;
+		
+		return [x1, y1, x2, y2, x3, y3, x4, y4];
+	},
+
+	// Source: https://gist.github.com/shamansir/3007244
+	edgeTest:function (p1, p2, p3, r2) {
+		var rot = [ -(p2[1] - p1[1]),
+					  p2[0] - p1[0] ];
+
+		var ref = (rot[0] * (p3[0] - p1[0]) +
+				   rot[1] * (p3[1] - p1[1])) >= 0;
+
+		for (var i = 0, il = r2.length; i < il; i+=2) {
+			if (((rot[0] * (r2[i]   - p1[0]) +
+				  rot[1] * (r2[i+1] - p1[1])) >= 0) === ref) return false;
+		}
+
+		return true;
+	},
+
+	// source: https://gist.github.com/shamansir/3007244
+	isecRects: function (r1, r2) {
+		if (!r1 || !r2) throw new Error('Rects are not accessible');
+
+		var pn, px;
+		for (var pi = 0, pl = r1.length; pi < pl; pi += 2) {
+			pn = (pi === (pl - 2)) ? 0 : pi + 2; // next point
+			px = (pn === (pl - 2)) ? 0 : pn + 2;
+			if (OBB.edgeTest([r1[pi], r1[pi+1]],
+						 [r1[pn], r1[pn+1]],
+						 [r1[px], r1[px+1]], r2)) return false;
+		}
+		for (var pi = 0, pl = r2.length; pi < pl; pi += 2) {
+			pn = (pi === (pl - 2)) ? 0 : pi + 2; // next point
+			px = (pn === (pl - 2)) ? 0 : pn + 2;
+			if (OBB.edgeTest([r2[pi], r2[pi+1]],
+						 [r2[pn], r2[pn+1]],
+						 [r2[px], r2[px+1]], r1)) return false;
+		}
+		return true;
+	},
+
+	// Overwrite the seperate function for OBB collision detection
+	separate: function (body1, body2, processCallback, callbackContext, overlapOnly) {
+		if (!body1.enable || !body2.enable)
+		{
+			return false;
+		}
+
+		//  Is there a custom process callback? If it returns true then we can carry on, otherwise we should abort.
+		if (processCallback && processCallback.call(callbackContext, body1.sprite, body2.sprite) === false)
+		{
+			return false;
+		}
+
+		return OBB.checkIntersection(body1, body2);
+	},
+
+	// This function is used to overwrite the seperate function of a Phase.Arcade.World instance.
+	setOBB: function (aracadeRef) {
+		aracadeRef.separate = OBB.separate;
+	}
+
+};
